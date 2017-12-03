28e84ab1e062810c6aa6c075ba280dfa53e8cde8
manishsaraan <manish021js@gmail.com>
2017-12-03 19:04:53 +0530

    initial push

---
+++ src/index.test.js
@@ -0,0 +1,6 @@
+import expect from 'expect';
+describe('Our first test', () => {
+   it('should pass', () => {
+       expect(2+2).toEqual(4);
+   });
+});
---
+++ tools/strtMessage.js
@@ -0,0 +1,3 @@
+import colors from 'colors';
+/* eslint-disable no-console*/
+console.log("starting the server in dev mode...".green);
