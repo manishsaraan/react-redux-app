0810f8d918b035bca62502e4b0ea13fc2277b526
manishsaraan <manish021js@gmail.com>
2017-12-16 15:35:45 +0530

    handeling error on course save

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,6 +1,6 @@
 import * as types from './actionTypes';
 import courseApi from '../api/mockCourseApi';
-import {beginAjaxCall} from './ajaxStatusActions';
+import {beginAjaxCall, ajaxCallError} from './ajaxStatusActions';
 
 export function loadCourseSuccess(courses){
    return { type : types.LOAD_COURSES_SUCCESS, courses };
@@ -31,6 +31,7 @@ export function saveCourse(course){
           course.id ? dispatch(updateCourseSuccess(savedCourse)) :
                       dispatch(createCourseSuccess(savedCourse));
       }).catch(error => {
+         dispatch(ajaxCallError());
          throw error;
       })
    }
