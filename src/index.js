c42969f8a855c0f92762a345eb3c02430de4c7ac
manishsaraan <manish021js@gmail.com>
2018-01-11 22:44:52 -0800

    showing courses list with redux saga

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
+import { loadCourses } from './actions/courseActions';
 import routes from './routes';
 import './styles/styles.css' 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 //setup initial state here
 const store = configureStore();
-
+store.dispatch(loadCourses());
 render(
 	<Provider store={store}>
        <Router history={browserHistory} routes={routes} />
